1f5972be753ed4992e3942891c4470df133ff0d4
Luca Leon Happel <lucahappel99@gmx.de>
2023-11-12 01:58:31 +0100

    add: patches from the `loogle` project
    See: https://github.com/nomeata/loogle/blob/a31308cba9c2be37d62794a7e3feeb9b3d5d980f/flake.nix#L73

--- flake.nix
+++ flake.nix
@@ -4,38 +4,48 @@
   nixConfig.max-jobs = "auto"; # Allow building multiple derivations in parallel
   # nixConfig.keep-outputs = true; # Do not garbage-collect build time-only dependencies (e.g. clang)
 
-  inputs.lean.url = "github:leanprover/lean4?ref=v4.2.0";
-  # Here we will add our dependency on Mathlib
-  # I stole this from https://github.com/stites/templates/blob/492ffebb29b479d3ee85fa24beb214ecb227fbb0/lean4/flake.nix
-  inputs.mathlib4 = {
-    url = "github:leanprover-community/mathlib4?ref=v4.2.0";
-    flake = false;
-  };
-  inputs.std4 = {
-    url = "github:leanprover/std4?ref=v4.2.0";
-    flake = false;
-  };
-  inputs.aesop = {
-    url = "github:JLimperg/aesop?ref=v4.2.0";
-    flake = false;
-  };
-  inputs.quote = {
-    url = "github:leanprover-community/quote4";
-    flake = false;
-  };
-  inputs.proofWidgets4 = {
-    url = "github:leanprover-community/ProofWidgets4?ref=v0.0.22";
-    flake = false;
-  };
-  inputs.cli = {
-    url = "github:leanprover/lean4-cli?ref=v2.2.0-lv4.0.0";
-    flake = false;
-  };
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs =
+      {
+        url = "github:NixOS/nixpkgs";
+        follows = "lean/nixpkgs";
+      };
 
-  outputs = { self, lean, flake-utils, ... }@inputs: flake-utils.lib.eachDefaultSystem (system:
+    lean.url = "github:leanprover/lean4?ref=v4.2.0";
+
+    # Here we will add our dependency on Mathlib
+    # I stole this from https://github.com/stites/templates/blob/492ffebb29b479d3ee85fa24beb214ecb227fbb0/lean4/flake.nix
+    std4 = {
+      url = "github:leanprover/std4?ref=v4.2.0";
+      flake = false;
+    };
+    mathlib4 = {
+      url = "github:leanprover-community/mathlib4?ref=v4.2.0";
+      flake = false;
+    };
+    aesop = {
+      url = "github:JLimperg/aesop?ref=v4.2.0";
+      flake = false;
+    };
+    quote4 = {
+      url = "github:leanprover-community/quote4";
+      flake = false;
+    };
+    proofWidgets4 = {
+      url = "github:leanprover-community/ProofWidgets4?ref=v0.0.22";
+      flake = false;
+    };
+    cli = {
+      url = "github:leanprover/lean4-cli?ref=v2.2.0-lv4.0.0";
+      flake = false;
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, lean, flake-utils, ... }@inputs: flake-utils.lib.eachDefaultSystem (system:
     let
       leanPkgs = lean.packages.${system};
+      pkgs = nixpkgs.legacyPackages.${system};
       pkg = leanPkgs.buildLeanPackage {
         name = "MyPackage"; # must match the name of the top-level .lean file
         src = ./.;
@@ -45,11 +55,13 @@
         src = inputs.aesop;
         precompilePackage = true;
         deps = [ std4 ];
+        roots = [ "Aesop" ];
       };
-      quote = leanPkgs.buildLeanPackage {
+      quote4 = leanPkgs.buildLeanPackage {
         name = "Qq";
-        src = inputs.quote;
+        src = inputs.quote4;
         precompilePackage = true;
+        roots = [{ mod = "Qq"; glob = "one"; }];
       };
       cli = leanPkgs.buildLeanPackage {
         name = "Cli";
@@ -61,30 +73,79 @@
         src = inputs.std4;
         precompilePackage = true;
         deps = [ cli ];
+        roots = [{ mod = "Std"; glob = "one"; }];
       };
       proofWidgets4 = leanPkgs.buildLeanPackage {
         name = "ProofWidgets";
         src = inputs.proofWidgets4;
         precompilePackage = true;
         deps = [ std4 ];
+        roots = [ "ProofWidgets" ];
+        overrideBuildModAttrs = addFakeFiles {
+          "ProofWidgets.Compat" = [ "build/js/compat.js" ];
+          "ProofWidgets.Component.Basic" = [ "build/js/interactiveExpr.js" ];
+          "ProofWidgets.Component.GoalTypePanel" = [ "build/js/goalTypePanel.js" ];
+          "ProofWidgets.Component.Recharts" = [ "build/js/recharts.js" ];
+          "ProofWidgets.Component.PenroseDiagram" = [ "build/js/penroseDisplay.js" ];
+          "ProofWidgets.Component.Panel.SelectionPanel" = [ "build/js/presentSelection.js" ];
+          "ProofWidgets.Component.Panel.GoalTypePanel" = [ "build/js/goalTypePanel.js" ];
+          "ProofWidgets.Component.MakeEditLink" = [ "build/js/makeEditLink.js" ];
+          "ProofWidgets.Component.OfRpcMethod" = [ "build/js/ofRpcMethod.js" ];
+          "ProofWidgets.Component.HtmlDisplay" =
+            [ "build/js/htmlDisplay.js" "build/js/htmlDisplayPanel.js" ];
+          "ProofWidgets.Presentation.Expr" = [ "build/js/exprPresentation.js" ];
+        };
       };
-      mathlib = leanPkgs.buildLeanPackage {
+      mathlib4 = leanPkgs.buildLeanPackage {
         name = "Mathlib";
         src = inputs.mathlib4;
-        precompilePackage = true;
-        deps = [
-          aesop
-          quote
-          std4
-          proofWidgets4
-          # optional doc-gen4
+        roots = [{ mod = "Mathlib"; glob = "one"; }];
+        leanFlags = [
+          "-Dpp.unicode.fun=true"
+          "-DautoImplicit=false"
+          "-DrelaxedAutoImplicit=false"
         ];
+        deps = [ std4 quote4 aesop proofWidgets4 ];
+        overrideBuildModAttrs = addFakeFiles {
+          "Mathlib.Tactic.Widget.CommDiag" = [
+            "widget/src/penrose/commutative.dsl"
+            "widget/src/penrose/commutative.sty"
+            "widget/src/penrose/triangle.sub"
+            "widget/src/penrose/square.sub"
+          ];
+        };
       };
+
+      # addFakeFile can plug into buildLeanPackage’s overrideBuildModAttrs
+      # it takes a lean module name and a filename, and makes that file available
+      # (as an empty file) in the build tree, e.g. for include_str.
+      addFakeFiles = m: self: super:
+        if m ? ${super.name}
+        then
+          let
+            paths = m.${super.name};
+          in
+          {
+            src = pkgs.runCommandCC "${super.name}-patched"
+              {
+                inherit (super) leanPath src relpath;
+              }
+              (''
+                dir=$(dirname $relpath)
+                mkdir -p $out/$dir
+                if [ -d $src ]; then cp -r $src/. $out/$dir/; else cp $src $out/$leanPath; fi
+              '' + pkgs.lib.concatMapStringsSep "\n"
+                (p: ''
+                  install -D -m 644 ${pkgs.emptyFile} $out/${p}
+                '')
+                paths);
+          }
+        else { };
     in
     {
       packages = pkg // {
         inherit (leanPkgs) lean;
-      } // mathlib;
+      } // std4 // quote4 // aesop // proofWidgets4 // mathlib4;
 
       defaultPackage = pkg.modRoot;
     });
